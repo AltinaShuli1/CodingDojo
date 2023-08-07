caef643b539037d31c1b2759dc17ab55fe69ed06
AltinaShuIi <altinashuli2@gmail.com>
2023-08-07 18:11:14 +0200

    commit

---
+++ JavaOOP/helloHuman/src/main/java/com/example/hellohuman/controllers/HelloHuman.java
@@ -0,0 +1,37 @@
+package com.example.hellohuman.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.StringWriter;
+
+@RestController
+public class HelloHuman {
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam(required = false) String lastname,
+                           @RequestParam(value = "times", required = false) Integer times) {
+        if (times != null && times > 0) {
+            String output = "";
+            for (int i = 0; i < times; i++) {
+
+                if (name != null) {
+                    output += "Hello " + name + " ";
+
+                } else {
+                    output += "Hello Human ";
+                }
+            }
+            return output;
+        }
+        return "Hello human ";
+    }
+}
+
+
+
+
+
+
+
---
+++ JavaOOP/helloHuman/src/test/java/com/example/hellohuman/controllers/MainController.java
@@ -0,0 +1,4 @@
+package com.example.hellohuman.controllers;
+
+public class MainController {
+}
